7f816f20581b8998b210c897bbc65c83699260ca
lg188 <lg188@tfwno.gf>
2016-06-22 21:35:29 +0000

    fix syntax

--- 10 - block dels.sql
+++ 10 - block dels.sql
@@ -4,9 +4,10 @@
 
 create or replace trigger nights_watch
 before delete on auteur
+declare
        v_time number(2,0) := to_number(sysdate,'HH');
 begin
-      if(v_time is between '3' and '4' and deleting) then
+      if(v_time between '3' and '4') then
                raise_application_error(-2001,'verboden om de wall over te steken');
-      end if
+      end if;
 end;
