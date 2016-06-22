6c639c374a1ff51c5b24908c527f3639bf675fc7
lg188 <lg188@tfwno.gf>
2016-06-22 19:37:12 +0000

    fixed initialisation

--- 01 - totale prijs boeken.sql
+++ 01 - totale prijs boeken.sql
@@ -2,13 +2,12 @@
  * Print totale prijs van alle boeken
  */
 declare
-  v_prijs boek.prijs%type ;
+  v_prijs boek.prijs%type := 0 ;
 begin
   for rec
   in (select prijs from boek)
   loop
     v_prijs := v_prijs + rec.prijs;
-    dbms_output.putline(rec.prijs);
   end loop;
   dbms_output.put_line('de totale prijs is' || v_prijs );
 end;
