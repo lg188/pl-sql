8a3d1c6ccf75932660317fb813547d1964dcd0f6
lg188 <lg188@tfwno.gf>
2016-06-22 21:11:01 +0000

    fix syntax

--- 07 - aantal stuks.sql
+++ 07 - aantal stuks.sql
@@ -2,14 +2,17 @@
  * vind het aantel stuks en naam van het boek met isbn 1345001254
 */
 declare
-  v_naam boek.naam%type := "";
-  v_aantal caddy.aantal%type := 0;
+  v_titel boek.titel%type := '';
+  v_aantal caddy_boek.aantal%type := 0;
 begin
-  select boek.naam from boek into v_naam where boek.isbn = 1345001254;
+  select boek.titel into v_titel from boek where boek.isbn = 1345001254;
 
-  from rec in ( select aantal from caddy_boek where isbn = 1345001254 ) loop
+  for rec in ( select caddy_boek.aantal from caddy_boek where isbn = 1345001254 ) loop
        v_aantal := v_aantal + rec.aantal;
   end loop;
-
-  dbms_output.put_line(v_naam || ':' || v_aantal);
+  if(v_aantal != 0) then
+    dbms_output.put_line(v_titel || ':' || v_aantal);
+  else
+    raise_application_error(-2001, 'you done fucked up');
+  end if;
 end;
